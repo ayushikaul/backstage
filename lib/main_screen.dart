02012504f902a9aabe4d594201f57974592ab5ca
f20190374 <f2019034@goa.bits-pilani.ac.in>
2020-07-09 23:23:15 +0530

    Arvind Commits

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -4,14 +4,17 @@ import 'package:backstage/sample_data.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){Navigator.push(context,MaterialPageRoute(builder: (context) => 
-                    DetailsScreen(equipmentItem: EquipmentItem()),
-                    ),);
-                  },
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailsScreen(equipmentItem: EquipmentItem()),
+          ),
+        );
+      },
       child: CustomScrollView(
         slivers: <Widget>[
           SliverGrid(
@@ -22,30 +25,30 @@ class MainScreen extends StatelessWidget {
               childAspectRatio: 0.8,
             ),
             delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
+              (BuildContext context, int index) {
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: Container(
-                  padding: EdgeInsets.fromLTRB(7.0, 0.0, 7.0, 4.0),
-                  alignment: Alignment.bottomCenter,
-                  child: Text(
-                    equipments[index].title,
-                     style: kTitleTextStyle,
-                     ),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image:NetworkImage(equipments[index].imageUrl[0]),
-                      fit: BoxFit.contain,
-                      alignment: Alignment.topCenter,
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(7.0, 0.0, 7.0, 4.0),
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        equipments[index].title,
+                        style: kTitleTextStyle,
+                      ),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(equipments[index].imageUrl[0]),
+                          fit: BoxFit.contain,
+                          alignment: Alignment.topCenter,
+                        ),
+                        shape: BoxShape.rectangle,
+                        boxShadow: [BoxShadow(color: Colors.indigo[400])],
+                      ),
                     ),
-                    shape: BoxShape.rectangle,
-                    boxShadow:[BoxShadow(color: Colors.indigo[400])],
                   ),
-                ),
-                ),
-               );
+                );
               },
               childCount: equipments.length,
             ),
@@ -53,5 +56,5 @@ class MainScreen extends StatelessWidget {
         ],
       ),
     );
-  }                        
+  }
 }
